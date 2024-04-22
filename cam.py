ee9fc190dd47f5dc6f9037c59ab7188c58c956bc
Kardi <kgland06@gmail.com>
2024-04-23 02:56:58 +0700

    camera page

---
+++ cam.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from PIL import Image
+
+#start camera
+camera_image = st.camera_input("Camera")
+
+if camera_image:
+    #Create a pillow image instance
+    img = Image.open(camera_image)
+
+    #convert to grayscale
+    gray_image = img.convert("L")
+
+    #render the grayscale image on the webpage
+    st.image(gray_image)
--- web.py
+++ web.py
@@ -12,7 +12,14 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase Your Productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo... ", on_change=add_todo, key='new_todo')
+
+st.session_state
